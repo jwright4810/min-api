cc440d2f68b984b42ef21c024789622b6b160a55
Jarett Wright <jwtech8313@gmail.comgit confit --list>
2019-02-10 02:10:21 -0600

    added signin controller

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const knex = require('knex');
 const moment = require('moment');
 const goals = require('./controllers/goals') 
 const register = require('./controllers/register');
+const signin = require('./controllers/signin')
 const bcrypt = require('bcrypt-nodejs'); 
+
 
 const db = knex({
     client: 'pg',
@@ -26,6 +28,7 @@ app.use(cors());
 app.get('/', (req, res) => {res.send('it is working!')})
 app.put('/submitgoals', (req, res) => { goals.handleSubmitGoals(req, res, db) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt, date) })
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt) })
 app.listen(3000, ()=> {
     console.log('app is running on port 3000'); 
 })
